bfb19c608781a235a3e898be5b0e4110ab97aa07
vivekmishra172003 <vivekmishra172003@gmail.com>
2024-09-21 23:36:57 +0530

    project setup

--- fullstack/AuthandAuth/src/index.ts
+++ fullstack/AuthandAuth/src/index.ts
@@ -1,14 +1,42 @@
 import express, { Application, Request,Response } from "express";
 import cookieParser  from "cookie-parser";
 import bcrypt from 'bcrypt'; 
+import jwt from 'jsonwebtoken';
+
 const app:Application = express();
 const PORT:number=3000;
 
 //middleware
 app.use(cookieParser());
-
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 //routes
 
+//jwt
+app.get("/j",(req:Request,res:Response)=>{
+    let token =jwt.sign({email:"harsh@example.com"},"secrect");
+    res.cookie("token",token);
+    // console.log(token);
+    // res.end();
+})
+app.get("/read",(req:Request,res:Response)=>{
+    let data = jwt.verify(req.cookies.token,"secrect");
+    console.log(data);
+})
+
+//bcrupt
+app.get("/auth",(req:Request,res:Response)=>{
+    bcrypt.genSalt(10,function(err,salt){
+        bcrypt.hash("myPlaintextPassword",salt,(err,hash)=>{
+            console.log(hash);
+        })
+    })
+})
+app.get("/enc",(req:Request,res:Response)=>{
+    bcrypt.compare("myPlaintextPassword","$2b$10$WDwSE74HoF..sIb8HwCKw.Tfvix9B5UZO.rf0laonnj/5KzzhOVRC",(err,result)=>{
+        console.log(result);
+    })
+})
 
 
 app.get('/',(req:Request,res:Response)=>{
