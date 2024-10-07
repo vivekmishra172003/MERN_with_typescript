9e813e58724f4425a5875f15490386d57f5bff65
vivekmishra172003 <vivekmishra172003@gmail.com>
2024-10-07 15:40:28 +0530

    axios is done

--- cohort/week3.2_Dynamic_Backend_Authentication_Using_Express_Jwt_and_postgres_docker/src/index.ts
+++ cohort/week3.2_Dynamic_Backend_Authentication_Using_Express_Jwt_and_postgres_docker/src/index.ts
@@ -1,2 +1,16 @@
-import express from 'express'
+import express,{Request,Response} from 'express'
 import axios from 'axios'
+import path from 'path'
+
+
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'../views'))
+
+app.get('/',(req:Request,res:Response)=>{
+    res.render('index')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
